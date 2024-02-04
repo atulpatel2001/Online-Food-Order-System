dc739aea793ff3c5310c332ebac9a67b28306f58
atul-patel-silver <atulpatel2001a@gmail.com>
2024-02-04 19:01:35 +0530

    commit

---
+++ src/main/java/com/online/food/OnlineFoodOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.online.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderSystemApplication.class, args);
+	}
+
+}
